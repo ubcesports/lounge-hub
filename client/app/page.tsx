d21244e706e7ac9f5efb4c52df558e7b61acb047
joshhwuu <joshuawu2004@gmail.com>
2024-09-23 17:01:48 -0700

    it works now

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,45 @@
 "use client";
 
-import React, { useState }from "react";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [studentNumber, setStudentNumber] = useState<string>("");
   const [data, setData] = useState(null);
 
-  const fetchData = async () => {
+  const fetchData = async (studentNumber:string) => {
+    const url = `http://localhost:5000/gamer/${studentNumber}`;
     try {
-      const response = await fetch("http://localhost:5000/gamer/12345678");
+      const response = await fetch(url);
       console.log("response: ", response);
       const data = await response.json();
       console.log("data: ", data);
       setData(data);
     } catch (error) {
-      console.error("Error:", error);
+      console.log("Error:", error);
     }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetchData(studentNumber);
   };
 
   return <div>
     <h1>Page</h1>
     <p>Page content</p>
-    <button onClick={fetchData}>Create Gamer</button>
+    <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Student Number"
+          value={studentNumber}
+          onChange={(e) => setStudentNumber(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
     {data && <div>{JSON.stringify(data)}</div>}
+    <form>
+      <input type="text" placeholder="First Name" />
+      <button type="submit">Submit</button>
+    </form>
   </div>
 }
