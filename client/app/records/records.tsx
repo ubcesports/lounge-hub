17eb91c2f520f204ff7b99c17d538eaf3926f105
Jaden Hums <49542833+Jaden51@users.noreply.github.com>
2024-10-06 15:43:11 -0700

    add frontend skeleton (#26)

---
+++ client/app/records/records.tsx
@@ -0,0 +1,7 @@
+export default function Records() {
+  return (
+    <div>
+      <h1>Records</h1>
+    </div>
+  );
+}
---
+++ client/app/student-info/student-info.tsx
@@ -0,0 +1,7 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Student info</h1>
+    </div>
+  );
+}
