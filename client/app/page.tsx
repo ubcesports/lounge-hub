eded289752b51e295138cfcd339ffde2436393f1
Jaden Hums <jadenhums51@gmail.com>
2024-10-07 00:50:52 -0700

    add add user stuff

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import CheckIn from "./check-in/check-in";
 import Login from "./auth/login";
 import Map from "./lounge/map";
@@ -8,6 +8,7 @@ import StudentInfo from "./student-info/student-info";
 import Records from "./records/records";
 import Activity from "./lounge/activity";
 import PcInfo from "./lounge/pc-info";
+import AddUser from "./check-in/add-user";
 
 export default function Page() {
   return (
@@ -22,6 +23,7 @@ export default function Page() {
       </div>
       <div>
         <CheckIn />
+        <AddUser />
         <StudentInfo />
         <Records />
       </div>
