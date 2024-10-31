5971a8cd48faa55010a51c6e593957d0cd7bd022
Vicki <vickiszhang@gmail.com>
2024-10-30 20:49:52 -0700

    wip activities

--- client/src/app/lounge/activity.tsx
+++ client/src/app/lounge/activity.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { getRecentActivity } from "../../services/activity";
+
 export default function Activity() {
+  const [recentActivities , setRecentActivities]  = useState([])
+
+  useEffect(()=>{
+
+    const fetchActivities = async () => {
+      try {
+        const activities = await getRecentActivity();
+        setRecentActivities(activities);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchActivities();
+    const intervalId = setInterval(fetchActivities, 5000);
+    return () => clearInterval(intervalId);
+      
+  },[])
+
   return (
     <div>
-      <h1>Activity</h1>
+      <h1>Activity</h1> 
+      <ul>
+         {recentActivities.map((activity) => (
+          <li><ul>
+          {Object.entries(activity).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul></li>
+         ))}
+      </ul>
     </div>
   );
 }
