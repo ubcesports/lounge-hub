a71c74169adbb6dc57f5f803c477398315f1eebc
Vicki <vickiszhang@gmail.com>
2024-11-03 23:43:56 -0800

    change usestate to use store

--- client/src/app/lounge/activity.tsx
+++ client/src/app/lounge/activity.tsx
@@ -1,52 +1,29 @@
 import { useEffect, useState } from "react";
-import { getRecentActivity } from "../../services/activity";
+import { getRecentActivity, useFetchActivities } from "../../services/activity";
 import { getGamerProfile } from "../../services/gamer-profile";
+import useBoundStore from "../../store/store";
+import { Log } from "../../interfaces/log";
 
 export default function Activity() {
-  const [recentLogs, setRecentLogs] = useState([]);
+  useFetchActivities();
+  const logList = useBoundStore((state) => state.logList);
 
-  useEffect(() => {
-    const fetchActivities = async () => {
-      try {
-        const activities = await getRecentActivity();
+  function formatLogData(log: Log) {
+    const startedDate = new Date(log.startedAt);
+    const endedDate = log.endedAt ? new Date(log.endedAt) : null;
 
-        const activitiesWithProfiles = await Promise.all(
-          activities.map(async (activity) => {
-            const profile = await getGamerProfile(activity.student_number);
-            return {
-              first_name: profile.first_name,
-              last_name: profile.last_name,
-              ...activity,
-            };
-          }),
-        );
-        setRecentLogs(activitiesWithProfiles);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchActivities();
-
-    const intervalId = setInterval(fetchActivities, 5000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  function formatLogData(log) {
-    const startedDate = new Date(log.started_at);
-    const endedDate = log.ended_at ? new Date(log.ended_at) : null;
-
-    const dateFormatted = `${String(startedDate.getMonth() + 1).padStart(2, "0")}-` +
-                            `${String(startedDate.getDate()).padStart(2, "0")}-` +
-                            `${startedDate.getFullYear()}`;
+    const dateFormatted =
+      `${String(startedDate.getMonth() + 1).padStart(2, "0")}-` +
+      `${String(startedDate.getDate()).padStart(2, "0")}-` +
+      `${startedDate.getFullYear()}`;
     const startTimeFormatted = startedDate.toTimeString().slice(0, 5);
     const endTimeFormatted = endedDate?.toTimeString().slice(0, 5);
 
-    return `${log.student_number} ・ 
+    return `${log.studentNumber} ・ 
             ${dateFormatted} ・ 
             Started ${startTimeFormatted} ・ 
             Ended ${endTimeFormatted || "N/A"} ・ 
-            ${log.pc_number} ・ 
+            ${log.pcNumber} ・ 
             ${log.game}`;
   }
 
@@ -54,17 +31,15 @@ export default function Activity() {
     <div>
       <h1 className="text-xl">Activity Log</h1>
       <div className="bg-gray-900 p-4">
-        {recentLogs.length === 0 && 
-        <div className="mb-4 border-b border-gray-700 pb-4">
-            <h1 className="text-xl text-white">
-              No Recent Activity
-            </h1>
-          </div>}
-        {recentLogs.map((log) => (
-          // TODO: fix unique key warning
+        {logList.length === 0 && (
           <div className="mb-4 border-b border-gray-700 pb-4">
+            <h1 className="text-xl text-white">No Recent Activity</h1>
+          </div>
+        )}
+        {logList.map((log, index) => (
+          <div key={index} className="mb-4 border-b border-gray-700 pb-4">
             <h1 className="text-xl text-white">
-              {log.first_name} {log.last_name}
+              {log.firstName} {log.lastName}
             </h1>
             <p className="text-sm text-gray-400">{formatLogData(log)}</p>
           </div>
---
+++ client/src/store/log-store.ts
@@ -0,0 +1,45 @@
+import { StateCreator } from "zustand";
+import { Log } from "../interfaces/log";
+
+export interface LogsSlice {
+  logList: Log[];
+  setLogList: (payload: Log[]) => void;
+  removeLogList: () => void;
+}
+
+const initialLogState: Log[] = [];
+
+const updateLogList = (data: any) => {
+  const updatedLogList = data.map((log) => {
+    return {
+      firstName: log.first_name,
+      lastName: log.last_name,
+      studentNumber: log.student_number,
+      game: log.game,
+      pcNumber: log.pc_number,
+      startedAt: log.started_at,
+      endedAt: log.ended_at,
+    };
+  });
+
+  return updatedLogList;
+};
+
+export const createRecentActivitySlice: StateCreator<LogsSlice> = (
+  set,
+  get,
+) => ({
+  logList: initialLogState,
+  setLogList: (payload: any) => {
+    const newLogList = updateLogList(payload);
+    const currentLogList = get().logList;
+
+    const hasChanged =
+      JSON.stringify(currentLogList) !== JSON.stringify(newLogList);
+
+    if (hasChanged) {
+      set({ logList: newLogList });
+    }
+  },
+  removeLogList: () => set({ logList: initialLogState }),
+});
