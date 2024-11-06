938fc4657f478b68bfd2cd7c1e0a0c95cff3cb92
Nic Ung <73774414+nicholasung@users.noreply.github.com>
2024-11-05 21:36:54 -0800

    Login component (#50)
    * added non functional components
    
    * simple state based implementation
    
    * moved into login.tsx
    
    * redundant login header
    
    * notes
    
    * fixed types

--- client/src/app/auth/login.tsx
+++ client/src/app/auth/login.tsx
@@ -1,7 +1,51 @@
-export default function Login() {
+import { ChangeEvent, FormEvent, useState } from "react";
+import Button from "../components/button";
+import TextField from "../components/text-field";
+
+const Login: React.FC = () => {
+  interface LoginFormState {
+    username: string;
+    password: string;
+  }
+  const [formState, setFormState] = useState<LoginFormState>({
+    username: "",
+    password: "",
+  });
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>): void {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  }
+
+  function handleLogin(e: FormEvent<Element>): void {
+    e.preventDefault();
+    console.log("handleLogin not implemented");
+    //call the User Authenitcation API
+  }
   return (
     <div>
       <h1>Log in</h1>
+      <form>
+        <TextField
+          label="Username"
+          name="username"
+          value={formState.username}
+          onChange={handleInputChange}
+        />
+        <TextField //todo change the component to obfuscate the text
+          label="Password"
+          name="password"
+          value={formState.password}
+          type="password"
+          onChange={handleInputChange}
+        />
+      </form>
+      <Button onClick={handleLogin} label="Authenticate" type="submit" />
     </div>
   );
-}
+};
+
+export default Login;
