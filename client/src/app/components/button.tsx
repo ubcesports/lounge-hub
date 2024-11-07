ddde4b08aa2f188f56060d8f147966cb811b99d9
joshhwuu <joshuawu2004@gmail.com>
2024-11-06 16:30:33 -0800

    prettier -_-

--- client/src/app/components/button.tsx
+++ client/src/app/components/button.tsx
@@ -9,7 +9,11 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ onClick, label, className, type }) => {
   return (
-    <button onClick={onClick} type={type} className={`rounded px-4 py-2 ${className}`}>
+    <button
+      onClick={onClick}
+      type={type}
+      className={`rounded px-4 py-2 ${className}`}
+    >
       {label}
     </button>
   );
