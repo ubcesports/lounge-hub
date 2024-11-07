5e11242fc96499f207b87a7bc3d87de68a689700
joshhwuu <joshuawu2004@gmail.com>
2024-11-06 16:29:54 -0800

    add type to button

--- client/src/app/components/button.tsx
+++ client/src/app/components/button.tsx
@@ -4,11 +4,12 @@ interface ButtonProps {
   onClick: (e: FormEvent) => void;
   label: string;
   className?: string;
+  type?: "button" | "submit" | "reset";
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, label, className = "" }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, label, className, type }) => {
   return (
-    <button onClick={onClick} className={`rounded px-4 py-2 ${className}`}>
+    <button onClick={onClick} type={type} className={`rounded px-4 py-2 ${className}`}>
       {label}
     </button>
   );
