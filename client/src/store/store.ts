0d35f7c53266c1b17edb58b8bf9a1c0a0f627008
devr07 <124638654+devr07@users.noreply.github.com>
2025-02-28 22:31:52 -0800

    Fixed only store PCSlices
    Was prev storing more than just PCSlices. Fixed that and refactored to only use middleware in combined store per zustand documentation

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -4,12 +4,18 @@ import { createGamerProfileSlice, GamerProfileSlice } from "./gamer-store";
 import { createPCSlice, PCSlice } from "./pc-store";
 import { createLogsSlice, LogsSlice } from "./log-store";
 
-const useStore = create<GamerProfileSlice & PCSlice & LogsSlice>()((...a) => ({
-  ...createGamerProfileSlice(...a),
-  ...persist(createPCSlice, {
-    name: "pc-store",
-  })(...a),
-  ...createLogsSlice(...a),
-}));
+const useStore = create<GamerProfileSlice & PCSlice & LogsSlice>()(
+  persist(
+    (...a) => ({
+      ...createGamerProfileSlice(...a),
+      ...createLogsSlice(...a),
+      ...createPCSlice(...a),
+    }),
+    {
+      name: "pc-store",
+      partialize: (state) => ({ PCList: state.PCList }), // Persist only PCList
+    }
+  )
+);
 
 export default useStore;
