ce49f36a4481eef58b5e4901a67f90a994fe9d63
Vicki <vickiszhang@gmail.com>
2025-03-24 21:55:57 -0700

    added overtime info

--- client/src/app/lounge/pc-station.tsx
+++ client/src/app/lounge/pc-station.tsx
@@ -7,7 +7,11 @@ interface PCStationProps {
   onClick: (pc: PC, timeRemaining: string, pcStatus: PCStatus) => void;
 }
 
-const PCStation: React.FC<PCStationProps> = ({ pc, pcStatus, onClick }) => {
+const PCStation: React.FC<PCStationProps> = ({
+  pc,
+  pcStatus,
+  onClick,
+}) => {
   const [timeRemaining, setTimeRemaining] = useState<string>("");
 
   const calculateTimeRemaining = (
