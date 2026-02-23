63b74a1ef78d5968965568a5df8a5eb7d28d3c0e
Jaden Hums <jadenhums51@gmail.com>
2026-02-22 20:22:47 -0800

    change dev environment to prep for echo-base

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,3 @@
-const API_URL = process.env.API_URL;
-
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${API_URL}/api/:path*`,
-      },
-    ];
-  },
   output: "standalone",
 };
